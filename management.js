81a2bec843b5795833d763a4f60e74c6859234d5
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-10-31 06:26:53 -0700

    First prompt works. Also is able to link to the management_db database and display the information based on the user selected choice

--- management.js
+++ management.js
@@ -3,6 +3,29 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 
+// Information to connect to the MySQL database
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "$n@k3t0wn!$myH0m",
+    database: "management_db"
+});
+
+
+// Establishes connection to database. Throws error if there is an issue
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+
 //User is prompted for what to do
 let userChoice = " ";
 function getUserChoice() {
@@ -10,7 +33,7 @@ function getUserChoice() {
         .prompt([
             {
                 type: "checkbox",
-                message: "What woud you like to do?",
+                message: "What would you like to do?",
                 name: "userChoice",
                 choices: [
                     "View All Employees",
@@ -30,7 +53,22 @@ function getUserChoice() {
             userChoice = (answers.userChoice[0]);
 
             console.log(userChoice)
+
+            if (userChoice === "View All Employees") {
+                displayEmployeeTable();
+            }
+
         })
 };
 
 getUserChoice();
+
+
+// Function to display the Employee Table from the management_db database
+function displayEmployeeTable() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    });
+};
