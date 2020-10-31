db786abc73659b86437bc92e96565ad886d82ce7
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-10-31 06:05:33 -0700

    First user prompt is done. Gives multiple options for the user to do something

--- management.js
+++ management.js
@@ -2,3 +2,35 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+
+//User is prompted for what to do
+let userChoice = " ";
+function getUserChoice() {
+    inquirer
+        .prompt([
+            {
+                type: "checkbox",
+                message: "What woud you like to do?",
+                name: "userChoice",
+                choices: [
+                    "View All Employees",
+                    "View All Employees by Department",
+                    "View All Employees by Manager",
+                    "Add Employee",
+                    "Remove Employee",
+                    "Update Employee Role",
+                    "Update Employee Manager",
+                    "View All Roles",
+                    "Add Role",
+                    "Remove Role"
+                ]
+            },
+        ])
+        .then(function (answers) {
+            userChoice = (answers.userChoice[0]);
+
+            console.log(userChoice)
+        })
+};
+
+getUserChoice();
