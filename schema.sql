be25526cf407e61ff2361be362c4ce7c27051cd6
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-10-31 05:33:54 -0700

    Completed the database in MySQL Workbench

--- schema.sql
+++ schema.sql
@@ -8,14 +8,14 @@ USE management_db;
 
 -- Creates the table "department" within management_db --
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30),
+  id INT AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(30),
   PRIMARY KEY(id)
 );
 
 -- Creates the table "role" within management_db --
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
@@ -24,13 +24,36 @@ CREATE TABLE role (
 
 -- Creates the table "role" within management_db --
 CREATE TABLE employee (
-  id INT NOT NULL,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
   PRIMARY KEY(id)
 );
 
 
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES ("Sales"), ("Engineering"), ("Finance"), ("Accounting"), ("Production"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Manager", 95000, 1), ("Sales Associate", 65000, 1), 
+		("Engineering Manager", 105000, 2), ("Software Engineer", 75000, 2), ("Mechanical Engineer", 65000, 2),
+        ("Financial Controller", 95000, 3),
+        ("Accounts Payable Rep", 55000, 4), ("Account Receivable Rep", 55000, 4),
+        ("Production Manager", 75000, 5), ("Production Supervisor", 55000, 5), ("Production Technician", 35000, 5),
+        ("Lawyer", 120000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+		("Mike", "Chan", 2), ("Bob", "Dole", 2),
+        ("Ashely", "Bonkers", 3),
+        ("Kevin", "Crazy Man", 4),
+        ("Tupik", "Sarnick", 5),
+        ("Mary", "Larry", 6),
+        ("Mike", "Bike", 7), 
+        ("Walter", "Saxphone", 8),
+        ("Tom", "Allen", 9),
+        ("Mike", "Brown", 10),
+        ("Kevin", "Bacon", 11), ("Marry", "Poppins", 11),
+        ("Liar", "Liar", 12);
